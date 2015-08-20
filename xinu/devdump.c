bb9ae1119ff848fbfa7b4572723b18bf71956d44
Dan Cross <cross@gajendra.net>
2015-08-20 18:36:30 -0400

    Fix a broken format string.

--- xinu/devdump.c
+++ xinu/devdump.c
@@ -12,7 +12,7 @@ devdump(void)
 	kprintf("--- --------  ----- -------- -------- -------- ----------\n");
 	for (int k = 0; k < NDEVS; k++) {
 		struct devsw *dp = &devtab[k];
-		kprintf("%2d. %-9s %3d   %08x %08x %08  %08x\n",
+		kprintf("%2d. %-9s %3d   %08x %08x %08x %08x\n",
 			k, dp->name, dp->minor,
 			dp->csr, dp->ivec, dp->ovec,
 			dp->iobuf);
